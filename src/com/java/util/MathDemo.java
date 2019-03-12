605da4c55ba066f0d63fa8e4e9992b111f042c0f
Kchenyuhang <kchengyuhang@gmail.com>
2019-03-12 10:58:56 +0800

    数学类的程序

---
+++ src/com/java/util/MathDemo.java
@@ -0,0 +1,15 @@
+package com.java.util;
+
+
+/**
+ * 数学类的程序
+ * @author 陈宇航
+ * 2019.3.12
+ */
+public class MathDemo {
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println(Math.sqrt(num));
+        System.out.printf("%.2f",Math.sqrt(num));
+    }
+}
